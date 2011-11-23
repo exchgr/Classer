964c70c9b4cc6eca9bdcef713c9dcf49ef9480ad
Dan Mundy <drm.mundy@gmail.com>
2011-11-22 19:23:53 -0500

    Tests MySQL queries

--- api/get/course.php
+++ api/get/course.php
@@ -1,5 +1,5 @@
 <?
 	$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
 	mysql_select_db("classer", $mySQLConnection);
-	echo json_encode(mysql_fetch_row(mysql_query("SELECT * FROM `courses` LIMIT 0")));
+	echo json_encode(mysql_fetch_row(mysql_query("SELECT * FROM `courses` LIMIT 1")));
 ?>
