ed4a32c6665608c8c99ba03e705857978a131af0
Dan Mundy <drm.mundy@gmail.com>
2011-11-22 23:43:57 -0500

    Has course.php get the course based on the contents of URL argument c.

--- api/get/course.php
+++ api/get/course.php
@@ -1,6 +1,6 @@
 <?
-	echo $_GET["c"];
 	$c = explode(" ", $_GET["c"]);
+	echo $c[0] . " " . $c[1];
 	$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
 	mysql_select_db("classer_tcnj", $mySQLConnection);
 	echo json_encode(mysql_fetch_assoc(mysql_query("SELECT * FROM `courses` WHERE `subj_abbr` = " . $c[0] . " AND `code` = " . $c[1] . " LIMIT 1")));
