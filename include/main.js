c9197b6400bceb3706e9224904a8b815c3bcddda
Dan Mundy <drm.mundy@gmail.com>
2011-12-02 10:24:42 -0500

    Implements a login system

--- include/main.js
+++ include/main.js
@@ -14,6 +14,34 @@ $(document).ready(function() {
 				$("form#login").html(localStorage.login.email);
 			} else {
 				$("form#login").html("<a href=\"#\" id=\"start\">Login</a>");
+				// Problem: jQuery apparently doesn't recognize this change, and
+				// completely ignores the fact that #start now exists. Also,
+				// this is why the .grey manips don't work in form#login.
+				$("form#login #start").click(function() {
+					$("form#login").html(
+						"<input type=\"text\" class=\"grey\" id=\"email\" name=\"email\" value=\"E-mail\">" + 
+						"<br /><input type=\"password\" class=\"grey\" id=\"password\" name=\"password\" value=\"Password\"> " + 
+						"<input type=\"submit\" value=\"\">"
+					);
+					$(".grey").focus(function() {
+						$(this).attr("value", "");
+						$(this).removeClass("grey");
+					});
+					$("form#login #email").blur(function() {
+						if ($(this).attr("value") === "") {
+							$(this).attr("value", "E-Mail");
+							$(this).addClass("grey");
+						}
+					});
+
+					$("form#login #password").blur(function() {
+						if ($(this).attr("value") === "") {
+							$(this).attr("value", "Password");
+							$(this).addClass("grey");
+						}
+					});
+					return false;
+				});
 			}
 		}
 	});
@@ -30,29 +58,6 @@ $(document).ready(function() {
 			$(this).attr("value", "Search");
 			$(this).addClass("grey");
 		}
-	});
-
-	$("form#login #email").blur(function() {
-		if ($(this).attr("value") === "") {
-			$(this).attr("value", "E-Mail");
-			$(this).addClass("grey");
-		}
-	});
-
-	$("form#login #password").blur(function() {
-		if ($(this).attr("value") === "") {
-			$(this).attr("value", "Password");
-			$(this).addClass("grey");
-		}
-	});
-
-	$("form#login #start").click(function() {
-		$("form#login").html(
-			"<input type=\"text\" class=\"grey\" id=\"email\" name=\"email\" value=\"E-mail\">" + 
-			"<br /><input type=\"password\" class=\"grey\" id=\"password\" name=\"password\" value=\"Password\"> " + 
-			"<input type=\"submit\" value=\"\">"
-		);
-		return false;
 	});
 
 	$("form#login").submit(function() {
