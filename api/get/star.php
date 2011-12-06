8b820b9a459360bf308aad53f1598ec8ecc68aab
Dan Mundy <drm.mundy@gmail.com>
2011-12-06 13:38:22 -0500

    Implements starred courses.

---
+++ api/get/star.php
@@ -0,0 +1,19 @@
+<?
+	$c = explode($_GET["c"]);
+	$token = $_GET["token"];
+
+	$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
+	mysql_select_db("classer_tcnj", $mySQLConnection);
+
+	$userQuery = "select email from users where token = '" + $token + "'";
+	$email = mysql_fetch_assoc(
+		mysql_query($userQuery);
+	);
+
+	$starQuery = "select subjAbbr, code from stars where email = '" + $email + "' and subjAbbr = '" + $c[0] + "' and code = '" + $c[1] + "'";
+	$star = mysql_fetch_assoc(
+		mysql_query($starQuery);
+	);
+
+	echo json_encode($star == $c);
+?>
