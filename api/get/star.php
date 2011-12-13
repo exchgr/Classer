ab811c407e87433ef5fe5d489cb178a662976776
Dan Mundy <drm.mundy@gmail.com>
2011-12-13 13:38:12 -0500

    Implements error handling in /api/get/course.php and /api/get/star.php

--- api/get/star.php
+++ api/get/star.php
@@ -1,26 +1,42 @@
 <?
+	require_once("/include/error.php");
+
 	$c = explode(" ", $_GET["c"]);
 	$token = $_GET["token"];
-
-	$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
-	mysql_select_db("classer_tcnj", $mySQLConnection);
 
 	function value($array) {
 		return $array[0];
 	}
 
-	$email = value(
-		mysql_fetch_array(
-			mysql_query("select email from users where token = '" . $token . "'"),
-			MYSQL_NUM
-		)
-	);
+	if ($token) {
+		if ($c) {
+			$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
+			mysql_select_db("classer_tcnj", $mySQLConnection);
 
-	$star = mysql_fetch_assoc(
-		mysql_query("select subjAbbr, code from stars where email = '" . $email . "' and subjAbbr = '" . $c[0] . "' and code = '" . $c[1] . "'")
-	);
+			$email = value(
+				mysql_fetch_array(
+					mysql_query("select email from users where token = '" . $token . "'"),
+					MYSQL_NUM
+				)
+			);
+
+			if ($email) {
+				$star = mysql_fetch_assoc(
+					mysql_query("select subjAbbr, code from stars where email = '" . $email . "' and subjAbbr = '" . $c[0] . "' and code = '" . $c[1] . "'")
+				);
+
+				echo json_encode((boolean) $star);
+			} else {
+				$error = new Error("401", "Bad token.");
+				$error->echoHeaderJSON();
+			}
+		} else {
+			$error = new Error("400", "c is undefined.");
+			$error->echoHeaderJSON();
+		}
+	} else {
+		
+	}
 
 	mysql_close($mySQLConnection);
-
-	echo json_encode((boolean) $star);
 ?>
