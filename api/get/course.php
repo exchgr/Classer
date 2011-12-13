18451bed9b098ec0cd54637b7a0a42f3ef71eea9
Dan Mundy <drm.mundy@gmail.com>
2011-12-13 13:54:18 -0500

    Implements error handling in /api/get/course.php and /api/get/star.php

--- api/get/course.php
+++ api/get/course.php
@@ -3,7 +3,7 @@
 
 	$c = explode(" ", $_GET["c"]);
 
-	if (empty($c)) {
+	if (!empty($c)) {
 		$mySQLConnection = mysql_connect("localhost", "classer", "cl4ssy");
 		mysql_select_db("classer_tcnj", $mySQLConnection);
 
@@ -13,7 +13,7 @@
 			)
 		);
 
-		if (empty($course)) {
+		if (!empty($course)) {
 			$satisfiesQuery = mysql_query("select satisfies from courses_satisfies where subjAbbr = (select subjAbbr from courses where subjAbbr = '" . $course["subjAbbr"] . "' and code = '" . $course["code"] . "') and code = (select code from courses where subjAbbr = '" . $course["subjAbbr"] . "' and code = '" . $course["code"] . "')");
 			$course["satisfies"] = array();
 			while ($satisfies = mysql_fetch_array($satisfiesQuery)) {
