e5493c340613d38c893429ded2cf9a0e6f83e6c7
Dan Mundy <drm.mundy@gmail.com>
2012-01-17 00:54:35 -0500

    This is a test

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <? require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php"); ?>
 <div class="wrapper">
 	<div id="main">
-		<h2>Construction Zone</h2>
+		<h2>THIS CHANGED LOL Construction Zone</h2>
 		<p>This site is under <a href="#">construction</a>. As it doesn't do anything for real yet, feel free to poke around. Be careful, and wear a hard hat.</p>
 	</div><!--/#main-->
 </div><!--/.wrapper-->
